418a049cbb0e56515e38422728fb698879f44a51
Laura Ozolina <laur755v@stud.kea.dk>
2023-12-05 20:59:31 +0100

    fix: updated the fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,10 @@ const HomePage = () => {
   return (
     <Layout>
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>CAPANNA47</h1>
+      <h2>Welcome to THE Home Page</h2>
+      <h3>hello HELLO world</h3>
+      <p>hello HELLO</p>
       {/* Other content */}
     </div></Layout>
   );
