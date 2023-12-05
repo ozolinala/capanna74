7ca84bdbe6e865e8361454b87dd68a822d8fe35a
Laura Ozolina <laur755v@stud.kea.dk>
2023-12-05 19:49:05 +0100

    fix: menu looks better

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,35 @@
 import Link from 'next/link';
 import React from 'react';
+import styles from '../styles/Layout.module.css'; // Import a separate CSS module
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <nav>
-        <Link href="/" passHref>
-          home/logo
-        </Link>{' '}
-        <Link href="/bookings" passHref>
-          BOOK A TABLE
-        </Link>
-        <Link href="/menu" passHref>
-          MENU
-        </Link>
-        <Link href="/takeaway" passHref>
-          TAKEAWAY
-        </Link>
-        <Link href="/our-story" passHref>
-          OUR STORY
-        </Link>
-        <Link href="/contact" >
-          CONTACT
-        </Link>
+    <div className={styles.container}>
+      <nav className={styles.nav}>
+        <div className={styles.leftNav}>
+          {/* Red text and left-aligned links */}
+          <Link href="/" passHref>
+            <p className={styles.redText}>CAPANNA47</p>
+          </Link>
+          <Link href="/bookings" passHref>
+            <p>BOOK A TABLE</p>
+          </Link>
+        </div>
+        <div className={styles.rightNav}>
+          {/* Right-aligned links */}
+          <Link href="/menu" passHref>
+            <p>MENU</p>
+          </Link>
+          <Link href="/takeaway" passHref>
+            <p>TAKEAWAY</p>
+          </Link>
+          <Link href="/our-story" passHref>
+            <p>OUR STORY</p>
+          </Link>
+          <Link href="/contact" passHref>
+            <p>CONTACT</p>
+          </Link>
+        </div>
       </nav>
       <main>{children}</main>
     </div>
