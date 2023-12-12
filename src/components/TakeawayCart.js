70ad1a6426bee78d675bf7749581a225b7c7fbb3
yejinyoun <yejinyoun@icloud.com>
2023-12-13 00:14:43 +0100

    takeaway fixed lilbit

--- src/components/TakeawayCart.js
+++ src/components/TakeawayCart.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 
 const TakeawayCart = ({ cartItems }) => {
   return (
@@ -11,6 +11,30 @@ const TakeawayCart = ({ cartItems }) => {
           </li>
         ))}
       </ul>
+    </div>
+  );
+};
+
+export default TakeawayCart; */
+
+import React from "react";
+
+const TakeawayCart = ({ cartItems }) => {
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+  };
+
+  return (
+    <div>
+      <h2>Your Order</h2>
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price} x {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p>Total: ${calculateTotal()}</p>
     </div>
   );
 };
--- src/components/TakeawayItem.js
+++ src/components/TakeawayItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const TakeawayItem = ({ item, onAddToCart }) => {
+/* const TakeawayItem = ({ item, onAddToCart }) => {
   const [quantity, setQuantity] = useState(0);
 
   const handleAddClick = () => {
@@ -23,6 +23,39 @@ const TakeawayItem = ({ item, onAddToCart }) => {
         {item.name} - ${item.price}
       </span>
       <button onClick={handleRemoveClick}>-</button>
+      <span>{quantity}</span>
+      <button onClick={handleAddClick}>+</button>
+    </li>
+  );
+};
+
+export default TakeawayItem; */
+
+const TakeawayItem = ({ item, onAddToCart }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleAddClick = () => {
+    const updatedQuantity = quantity + 1;
+    setQuantity(updatedQuantity);
+    onAddToCart({ ...item, quantity: updatedQuantity });
+  };
+
+  const handleRemoveClick = () => {
+    if (quantity > 0) {
+      const updatedQuantity = quantity - 1;
+      setQuantity(updatedQuantity);
+      onAddToCart({ ...item, quantity: updatedQuantity });
+    }
+  };
+
+  return (
+    <li>
+      <span>
+        {item.name} - ${item.price}
+      </span>
+      <button onClick={handleRemoveClick} disabled={quantity === 0}>
+        -
+      </button>
       <span>{quantity}</span>
       <button onClick={handleAddClick}>+</button>
     </li>
