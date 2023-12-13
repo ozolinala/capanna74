8bb6833dd74cf9c1a3d91dd1fa713c21cc3c3a90
Kai Aliani <kaialiani01@gmail.com>
2023-12-13 14:32:08 +0100

    Added grey to palette, changed navbar .active effect, changed global text color to grey

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import OurStoryGrid from '@/components/OurStoryGrid';
-import styles from '../styles/OurStory.module.css';
+import React from "react";
+import Layout from "../components/Layout";
+import OurStoryGrid from "@/components/OurStoryGrid";
+import styles from "../styles/OurStory.module.css";
 
 const OurStory = () => {
   return (
     <Layout currentPage="yourComponent">
-<OurStoryGrid></OurStoryGrid>
-<div className={styles.others}>
-      <h2 classNamestyle={{ color:'var(--accent-color)'}}>All about family</h2>
-      <p>
-        Currently, we’re a family of three. 
-        <br></br>Visit our sister restaurants
-        <br></br>
-        <a href="http://mangia.dk/" target="_blank" rel="noopener noreferrer">
-          Mangia
-        </a>
-        <span> and </span> 
-        <a href="https://www.ristorantecircolo.dk/" target="_blank" rel="noopener noreferrer">
-          Circolo
-        </a>.
-      </p>
-    </div>
+      <OurStoryGrid></OurStoryGrid>
+      <div className={styles.others}>
+        <h2 classNamestyle={{ color: "var(--accent-color)" }}>
+          All about family
+        </h2>
+        <p>
+          Currently, we’re a family of three.
+          <br></br>Visit our sister restaurants
+          <br></br>
+          <a href="http://mangia.dk/" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/Mangia-logo.svg"
+              alt="Mangia"
+              className={styles.mangiaLogo}
+            />
+          </a>
+          <span> & </span>
+          <a
+            href="https://www.ristorantecircolo.dk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/assets/Circolo-logo.svg"
+              alt="Circolo"
+              className={styles.circoloLogo}
+            />
+          </a>
+          .
+        </p>
+      </div>
     </Layout>
   );
 };
