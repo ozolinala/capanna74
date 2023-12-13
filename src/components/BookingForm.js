1faeea57af4685a4824ccf66180ddce16225002b
Kai Aliani <kaialiani01@gmail.com>
2023-12-13 23:14:00 +0100

    Fake flow working (with "reservation succesful" message, still missing media queries

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,12 +1,38 @@
 import styles from "../styles/Booking.module.css";
-import { Button, Form, Input, Space, TimePicker, DatePicker } from "antd";
+import { useState } from "react";
+import Link from "next/link";
+import {
+  Button,
+  Form,
+  Input,
+  Space,
+  TimePicker,
+  DatePicker,
+  Row,
+  Col,
+} from "antd";
 import dayjs from "dayjs";
 
 const BookingForm = () => {
   const { TextArea } = Input;
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      setSuccess(true);
+      console.log("Success:", values);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
@@ -14,142 +40,163 @@ const BookingForm = () => {
   const onChange = (date, dateString) => {
     console.log(date, dateString);
   };
+
   const format = "HH:mm";
+
   return (
-    <article className={styles.container}>
-      <div>
-        <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          style={{
-            maxWidth: 600,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="First name"
-            name="first-name"
-            rules={[
-              {
-                required: true,
-                message: "Please input your name!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Last name"
-            name="last-name"
-            rules={[
-              {
-                required: true,
-                message: "Please input your last name!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="E-mail"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Phone number"
-            name="phone-number"
-            rules={[
-              {
-                required: true,
-                message: "Please input your phone number",
-              },
-            ]}
-          >
-            <Space.Compact>
-              <Input
-                style={{
-                  width: "20%",
-                }}
-                defaultValue="+45"
-              />
-              <Input
-                style={{
-                  width: "80%",
-                }}
-                defaultValue="00 00 00 00"
-              />
-            </Space.Compact>
-          </Form.Item>
-
-          <Form.Item
-            label="Comment"
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              span: 16,
-            }}
-          >
-            <TextArea rows={4} />
-          </Form.Item>
-        </Form>
-      </div>
-      <div>
-        <Form>
-          <Form.Item
-            label="Select a date"
-            name="date"
-            valuePropName="checked"
-            wrapperCol={{
-              span: 16,
-            }}
-          >
-            <DatePicker onChange={onChange} />
-          </Form.Item>
-          <Form.Item
-            label="Select a time"
-            name="time"
-            valuePropName="checked"
-            wrapperCol={{
-              span: 16,
-            }}
-          >
-            <TimePicker
-              defaultValue={dayjs("19:30", format)}
-              format={format}
-              minuteStep={15}
-              hourStep={1}
+    <article>
+      <div className={styles.container}>
+        {success ? (
+          <div className={styles.successMessageContainer}>
+            <p className={styles.successMessage}>
+              The table is available, you have successfully made a reservation!
+            </p>
+            <img
+              src="/assets/check.svg"
+              alt="Landing Image"
+              className={styles.check}
             />
-          </Form.Item>
-          <Form.Item
+            <Link className={styles.backToMenu} href="/">
+              Go back to menu
+            </Link>
+          </div>
+        ) : (
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
             wrapperCol={{
               span: 16,
             }}
+            style={{
+              maxWidth: "90vw",
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+            <Row gutter={16} className="column">
+              <Col span={12}>
+                <Form.Item
+                  label="First name"
+                  name="first-name"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your name!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Last name"
+                  name="last-name"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your last name!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="E-mail"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your email!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Phone number"
+                  name="phone-number"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your phone number",
+                    },
+                  ]}
+                >
+                  <Space.Compact>
+                    <Input
+                      style={{
+                        width: "20%",
+                      }}
+                      defaultValue="+45"
+                    />
+                    <Input
+                      style={{
+                        width: "80%",
+                      }}
+                      defaultValue="00 00 00 00"
+                    />
+                  </Space.Compact>
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item
+                  label="Comment"
+                  name="remember"
+                  valuePropName="checked"
+                  wrapperCol={{
+                    span: 16,
+                  }}
+                >
+                  <TextArea rows={4} />
+                </Form.Item>
+
+                <Form.Item
+                  label="Select a date"
+                  name="date"
+                  valuePropName="checked"
+                  wrapperCol={{
+                    span: 16,
+                  }}
+                >
+                  <DatePicker onChange={onChange} />
+                </Form.Item>
+                <Form.Item
+                  label="Select a time"
+                  name="time"
+                  valuePropName="checked"
+                  wrapperCol={{
+                    span: 16,
+                  }}
+                >
+                  <TimePicker
+                    defaultValue={dayjs("19:30", format)}
+                    format={format}
+                    minuteStep={15}
+                    hourStep={1}
+                  />
+                </Form.Item>
+                <Form.Item
+                  wrapperCol={{
+                    span: 16,
+                    offset: 0,
+                  }}
+                >
+                  <Button type="primary" htmlType="submit" loading={loading}>
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        )}
       </div>
     </article>
   );
