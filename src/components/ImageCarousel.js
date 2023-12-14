b6b82fd1d16e358b26ac6cba05c50fb4ac7544bf
Kai Aliani <kaialiani01@gmail.com>
2023-12-14 02:41:50 +0100

    Carousel fully functional w/ autoplay in desktop view (just need to select the right pics)

---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "../styles/LandingGrid.module.css";
+
+const ImageCarousel = ({ images }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    variableWidth: true,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <div className={styles.carouselWrapper}>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.slide}>
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default ImageCarousel;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
-import LandingGrid from "@/components/LandingGrid";
+import ImageCarousel from "../components/ImageCarousel";
 
+const images = [
+  "/assets/contacts-image.jpg",
+  "/assets/Homepage-Picture1.png",
+  "/assets/our-story1.png",
+  "/assets/our-story2.png",
+  "/assets/our-story3.png",
+  "/assets/Homepage-Picture2.png",
+  "/assets/Homepage-Picture3.png",
+  "/assets/our-story4.png",
+  "/assets/our-story5.png",
+];
 
 const HomePage = () => {
   return (
     <Layout>
-
-      <LandingGrid />
+      <ImageCarousel images={images} />
     </Layout>
   );
 };
