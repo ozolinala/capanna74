7eb0efd0d9b186c51c3104169f14cb062532ab82
kaialiani <111850398+kaialiani@users.noreply.github.com>
2023-12-15 01:47:01 +0100

    Removed antdesign package, new form (also responsive)

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,32 +1,22 @@
 import styles from "../styles/Booking.module.css";
 import { useState } from "react";
 import Link from "next/link";
-import {
-  Button,
-  Form,
-  Input,
-  Space,
-  TimePicker,
-  DatePicker,
-  Row,
-  Col,
-} from "antd";
-const { useMediaQuery } = require("react-responsive");
-const dayjs = require("dayjs");
+import { useRouter } from "next/router";
 
 const BookingForm = () => {
-  const { TextArea } = Input;
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const router = useRouter();
 
-  const onFinish = async (values) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     setLoading(true);
 
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
       setSuccess(true);
-      console.log("Success:", values);
+      console.log("Success:", event.target.elements);
     } catch (error) {
       console.error("Error:", error);
     } finally {
@@ -34,333 +24,96 @@ const BookingForm = () => {
     }
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  const onChange = (date, dateString) => {
-    console.log(date, dateString);
-  };
-
-  const format = "HH:mm";
-
-  const isMobile = useMediaQuery({ maxWidth: 768 });
-
   return (
     <article>
-      {isMobile ? (
-        <div className={styles.containerMobile}>
-          {
-            <div className={styles.container}>
-              {success ? (
-                <div className={styles.successMessageContainer}>
-                  <p className={styles.successMessage}>
-                    You have successfully made a
-                    reservation!
-                  </p>
-                  <img
-                    src="/assets/check.svg"
-                    alt="Landing Image"
-                    className={styles.check}
+      <div>
+        {success ? (
+          <div className={styles.successMessageContainer}>
+            <p className={styles.successMessage}>
+              You have successfully made a reservation!
+            </p>
+            <img
+              src="/assets/check.svg"
+              alt="Checkmark"
+              className={styles.check}
+            />
+            <Link className={styles.backToMenu} href="/">
+              Go back to menu
+            </Link>
+          </div>
+        ) : (
+          <form
+            onSubmit={handleSubmit}
+            autoComplete="off"
+            className={styles.formContainer}
+          >
+            <div className={styles.leftColumn}>
+              <div>
+                <label htmlFor="first-name">First name:</label>
+                <input type="text" id="first-name" required />
+              </div>
+
+              <div>
+                <label htmlFor="last-name">Last name:</label>
+                <input type="text" id="last-name" required />
+              </div>
+
+              <div>
+                <label htmlFor="email">E-mail:</label>
+                <input type="email" id="email" required />
+              </div>
+
+              <div>
+                <label htmlFor="phone-number">Phone number:</label>
+                <div>
+                  <input
+                    type="text"
+                    id="phone-prefix"
+                    className={styles.phonePrefix}
+                    defaultValue="+45"
                   />
-                  <Link className={styles.backToMenu} href="/">
-                    Go back to menu
-                  </Link>
+                  <input
+                    type="text"
+                    id="phone-number"
+                    defaultValue="00 00 00 00"
+                  />
                 </div>
-              ) : (
-                <Form
-                  name="basic"
-                  style={{
-                    maxWidth: "90vw",
-                    marginLeft: "1rem",
-                  }}
-                  initialValues={{
-                    remember: true,
-                  }}
-                  onFinish={onFinish}
-                  onFinishFailed={onFinishFailed}
-                  autoComplete="off"
+              </div>
+            </div>
+
+            <div className={styles.rightColumn}>
+              <div>
+                <label htmlFor="comment">Comment:</label>
+                <textarea id="comment" rows="4" />
+              </div>
+
+              <div>
+                <label htmlFor="date">Select a date:</label>
+                <input
+                  type="date"
+                  id="date"
+                  onChange={(e) => console.log(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="time">Select a time:</label>
+                <input type="time" id="time" />
+              </div>
+
+              <div>
+                <button
+                  className={styles.button}
+                  type="submit"
+                  disabled={loading}
                 >
-                  <Form.Item
-                    label="First name"
-                    name="first-name"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your name",
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Last name"
-                    name="last-name"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your last name",
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="E-mail"
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your email",
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Phone number"
-                    name="phone-number"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your phone number",
-                      },
-                    ]}
-                  >
-                    <Space.Compact>
-                      <Input style={{ width: "20%" }} defaultValue="+45" />
-                      <Input
-                        style={{ width: "80%" }}
-                        defaultValue="00 00 00 00"
-                      />
-                    </Space.Compact>
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Comment"
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{ span: 16 }}
-                  >
-                    <TextArea rows={4} />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Select a date"
-                    name="date"
-                    valuePropName="checked"
-                    wrapperCol={{ span: 16 }}
-                  >
-                    <DatePicker onChange={onChange} />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Select a time"
-                    name="time"
-                    valuePropName="checked"
-                    wrapperCol={{ span: 16 }}
-                  >
-                    <TimePicker
-                      defaultValue={dayjs("19:30", format)}
-                      format={format}
-                      minuteStep={15}
-                      hourStep={1}
-                    />
-                  </Form.Item>
-
-                  <Form.Item wrapperCol={{ span: 24, offset: 0 }}>
-                    <div className={styles.buttonContainerMobile}>
-                      <Button
-                        style={{
-                          width: "10rem",
-                          borderRadius: "20px",
-                          backgroundColor: "#e85835",
-                          color: "white",
-                        }}
-                        type="primary"
-                        htmlType="submit"
-                        loading={loading}
-                      >
-                        Submit
-                      </Button>
-                    </div>
-                  </Form.Item>
-                </Form>
-              )}
+                  Submit
+                </button>
+              </div>
             </div>
-          }
-        </div>
-      ) : (
-        <div className={styles.container}>
-          {success ? (
-            <div className={styles.successMessageContainer}>
-              <p className={styles.successMessage}>
-                The table is available, you have successfully made a
-                reservation!
-              </p>
-              <img
-                src="/assets/check.svg"
-                alt="Landing Image"
-                className={styles.check}
-              />
-              <Link className={styles.backToMenu} href="/">
-                Go back to menu
-              </Link>
-            </div>
-          ) : (
-            <Form
-              name="basic"
-              labelCol={{
-                span: isMobile ? 16 : 8,
-              }}
-              wrapperCol={{
-                span: isMobile ? 16 : 16,
-              }}
-              style={{
-                maxWidth: "90vw",
-              }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Row gutter={16}>
-                <Col span={12}>
-                  <Form.Item
-                    label="First name"
-                    name="first-name"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your name",
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Last name"
-                    name="last-name"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your last name",
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="E-mail"
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your email",
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Phone number"
-                    name="phone-number"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your phone number",
-                      },
-                    ]}
-                  >
-                    <Space.Compact>
-                      <Input
-                        style={{
-                          width: "20%",
-                        }}
-                        defaultValue="+45"
-                      />
-                      <Input
-                        style={{
-                          width: "80%",
-                        }}
-                        defaultValue="00 00 00 00"
-                      />
-                    </Space.Compact>
-                  </Form.Item>
-                </Col>
-                <Col span={12}>
-                  <Form.Item
-                    label="Comment"
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
-                      span: 16,
-                    }}
-                  >
-                    <TextArea rows={4} />
-                  </Form.Item>
-
-                  <Form.Item
-                    label="Select a date"
-                    name="date"
-                    valuePropName="checked"
-                    wrapperCol={{
-                      span: 16,
-                    }}
-                  >
-                    <DatePicker onChange={onChange} />
-                  </Form.Item>
-                  <Form.Item
-                    label="Select a time"
-                    name="time"
-                    valuePropName="checked"
-                    wrapperCol={{
-                      span: 16,
-                    }}
-                  >
-                    <TimePicker
-                      defaultValue={dayjs("19:30", format)}
-                      format={format}
-                      minuteStep={15}
-                      hourStep={1}
-                    />
-                  </Form.Item>
-                  <Form.Item
-                    wrapperCol={{
-                      span: 24, // Set to the full width of the column
-                      offset: 0,
-                    }}
-                  >
-                    <div className={styles.buttonContainer}>
-                      <Button
-                        style={{
-                          width: "10rem",
-                          borderRadius: "20px",
-                          backgroundColor: "#e85835",
-                          color: "white",
-                        }}
-                        type="primary"
-                        htmlType="submit"
-                        loading={loading}
-                      >
-                        Submit
-                      </Button>
-                    </div>
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Form>
-          )}
-        </div>
-      )}
+          </form>
+        )}
+      </div>
     </article>
   );
 };
