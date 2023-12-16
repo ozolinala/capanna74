a2bf854b064c11439cfd39c554bb934bb34b5081
yejinyoun <yejinyoun@icloud.com>
2023-12-16 18:05:48 +0100

    cleaned

--- src/components/TakeawayCart.js
+++ src/components/TakeawayCart.js
@@ -1,22 +1,3 @@
-/* import React from "react";
-
-const TakeawayCart = ({ cartItems }) => {
-  return (
-    <div>
-      <h2>Your Order</h2>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            {item.name} - ${item.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default TakeawayCart; */
-
 import React from "react";
 
 const TakeawayCart = ({ cartItems }) => {
