fe223094b6cb6cbdf3fd12942c7108ebecb6b1a8
yejinyoun <yejinyoun@icloud.com>
2023-12-17 19:24:21 +0100

    cart items close/open

--- src/components/TakeawayCart.js
+++ src/components/TakeawayCart.js
@@ -3,17 +3,19 @@ import styles from "../styles/TakeawayMenu.module.css";
 import { useState, useEffect } from "react";
 
 const TakeawayCart = ({ cartItems, orderProcess, setOrderProcess }) => {
+  const [showFullList, setShowFullList] = useState(false);
+
+  const visibleItems = showFullList ? cartItems : cartItems.slice(-2);
+
   const calculateTotal = () => {
-    return cartItems
-      .reduce((total, item) => total + item.price * item.quantity, 0)
-      .toFixed(2);
+    return visibleItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
   };
 
   return (
     <div className={styles.cartContainer}>
       <h2>Your Order</h2>
       <ul className={styles.cartList}>
-        {cartItems.map((item) => (
+        {visibleItems.map((item) => (
           <li className={styles.cartListItem} key={item.id}>
             <div>
               {item.name} × {item.quantity}
@@ -22,6 +24,11 @@ const TakeawayCart = ({ cartItems, orderProcess, setOrderProcess }) => {
           </li>
         ))}
       </ul>
+      {cartItems.length > 2 && (
+        <button className={styles.showMoreButton} onClick={() => setShowFullList(!showFullList)}>
+          {showFullList ? "Show Less" : "Show More"}
+        </button>
+      )}
       <p className={styles.total}>
         Total: <b>{calculateTotal()}kr.</b>
       </p>
