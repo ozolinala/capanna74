4c9ca0d3e8f6274d9c078354dbcfd7c7bd3c9d9e
Kai Aliani <kaialiani01@gmail.com>
2023-12-18 00:49:26 +0100

    Fixed contact form responsiveness + number74 color changed + aligned button to center

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -46,105 +46,101 @@ const ContactForm = () => {
         ) : (
           <div>
             <form
- onSubmit={handleSubmit}
- autoComplete="off"
- className={styles.formContainer}
->
- <div className={styles.leftColumn}>
-  <div className={styles.nameContainer}>
-    <div>
-      <label
-        style={{
-          position: "absolute",
-          transform: "translateX(15px)",
-          backgroundColor: "#fff",
-          lineHeight: "0px",
-          paddingLeft: "10px",
-          paddingRight: "8px",
-        }}
-        htmlFor="first-name"
-      >
-        First name <span className={styles.star}>*</span>
-      </label>
-      <input type="text" id="first-name" required />
-    </div>
+              onSubmit={handleSubmit}
+              autoComplete="off"
+              className={styles.formContainer}
+            >
+              <div className={styles.nameContainer}>
+                <div>
+                  <label
+                    style={{
+                      position: "absolute",
+                      transform: "translateX(15px)",
+                      backgroundColor: "#fff",
+                      lineHeight: "0px",
+                      paddingLeft: "10px",
+                      paddingRight: "8px",
+                    }}
+                    htmlFor="first-name"
+                  >
+                    First name <span className={styles.star}>*</span>
+                  </label>
+                  <input type="text" id="first-name" required />
+                </div>
 
-    <div>
-      <label
-        style={{
-          position: "absolute",
-          transform: "translateX(15px)",
-          backgroundColor: "#fff",
-          lineHeight: "0px",
-          paddingLeft: "10px",
-          paddingRight: "8px",
-        }}
-        htmlFor="last-name"
-      >
-        Last name <span className={styles.star}>*</span>
-      </label>
-      <input type="text" id="last-name" required />
-    </div>
-  </div>
- </div>
+                <div>
+                  <label
+                    style={{
+                      position: "absolute",
+                      transform: "translateX(15px)",
+                      backgroundColor: "#fff",
+                      lineHeight: "0px",
+                      paddingLeft: "10px",
+                      paddingRight: "8px",
+                    }}
+                    htmlFor="last-name"
+                  >
+                    Last name <span className={styles.star}>*</span>
+                  </label>
+                  <input type="text" id="last-name" required />
+                </div>
+              </div>
 
- <div className={styles.rightColumn}>
-  <div className={styles.inputContainer}>
-    <label
-      style={{
-        position: "absolute",
-        transform: "translateX(15px)",
-        backgroundColor: "#fff",
-        lineHeight: "1px",
-        paddingLeft: "10px",
-        paddingRight: "8px",
-      }}
-      htmlFor="email"
-    >
-      E-mail<span className={styles.star}>*</span>
-    </label>
-    <input type="email" id="email" />
-  </div>
-  <div className={styles.inputContainer}>
-    <label
-      style={{
-        position: "absolute",
-        transform: "translateX(15px)",
-        backgroundColor: "#fff",
-        lineHeight: "1px",
-        paddingLeft: "10px",
-        paddingRight: "8px",
-      }}
-      htmlFor="email"
-    >
-      Mobile<span className={styles.star}>*</span>
-    </label>
-    <input type="email" id="email" />
-  </div>
-  <div className={styles.inputContainer}>
-    <label
-      style={{
-        position: "absolute",
-        transform: "translateX(15px)",
-        backgroundColor: "#fff",
-        lineHeight: "1px",
-        paddingLeft: "10px",
-        paddingRight: "8px",
-      }}
-      htmlFor="comment"
-    >
-      Message <span className={styles.star}>*</span>
-    </label>
-    <textarea id="comment" rows="4" />
-  </div>
+              <div className={styles.inputContainer}>
+                <label
+                  style={{
+                    position: "absolute",
+                    transform: "translateX(15px)",
+                    backgroundColor: "#fff",
+                    lineHeight: "1px",
+                    paddingLeft: "10px",
+                    paddingRight: "8px",
+                  }}
+                  htmlFor="email"
+                >
+                  E-mail<span className={styles.star}>*</span>
+                </label>
+                <input type="email" id="email" />
+              </div>
+              <div className={styles.inputContainer}>
+                <label
+                  style={{
+                    position: "absolute",
+                    transform: "translateX(15px)",
+                    backgroundColor: "#fff",
+                    lineHeight: "1px",
+                    paddingLeft: "10px",
+                    paddingRight: "8px",
+                  }}
+                  htmlFor="email"
+                >
+                  Mobile<span className={styles.star}>*</span>
+                </label>
+                <input type="email" id="email" />
+              </div>
+              <div className={styles.inputContainer}>
+                <label
+                  style={{
+                    position: "absolute",
+                    transform: "translateX(15px)",
+                    backgroundColor: "#fff",
+                    lineHeight: "1px",
+                    paddingLeft: "10px",
+                    paddingRight: "8px",
+                  }}
+                  htmlFor="comment"
+                >
+                  Message <span className={styles.star}>*</span>
+                </label>
+                <textarea id="comment" rows="4" />
+              </div>
 
-  <div className={styles.buttoncontainer}>
-    <button className={styles.button} type="submit">
-      Submit
-    </button>
-  </div>
- </div>
-</form>
+              <div className={styles.buttoncontainer}>
+                <button className={styles.button} type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
         )}
       </div>
