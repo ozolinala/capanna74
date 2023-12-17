c0b2cd738649ff1d2b83929a87d3cb3cad06cbd3
yejinyoun <yejinyoun@icloud.com>
2023-12-17 19:35:40 +0100

    button styling

--- src/components/TakeawayCart.js
+++ src/components/TakeawayCart.js
@@ -26,7 +26,35 @@ const TakeawayCart = ({ cartItems, orderProcess, setOrderProcess }) => {
       </ul>
       {cartItems.length > 2 && (
         <button className={styles.showMoreButton} onClick={() => setShowFullList(!showFullList)}>
-          {showFullList ? "Show Less" : "Show More"}
+          {showFullList ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#FFFFFF"
+              class="bi bi-chevron-up"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="#FFFFFF"
+              class="bi bi-chevron-down"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          )}
         </button>
       )}
       <p className={styles.total}>
